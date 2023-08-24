339f80478852fd54603331925a30158805c7745f
dvera17 <dsvera174@gmail.com>
2023-08-24 17:19:17 -0500

    feat: :sparkles: Implementacion y utilizacion de los endpoints
    Implementacion y utilizacion de los endpoints

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import { config } from "dotenv";
+import routerLogin from './routes/login.routes.js';
+import routerArea from './routes/area.routes.js';
+import passportHelper from './config/passportConfig.js';
 config();
 const app = express();
 app.set('port', process.env.PORT_SERVER);
 app.use(express.json());
+app.use(passportHelper.initialize());
+const bearerAuth = passportHelper.authenticate('bearer', { session: false })
+
+app.use('/login', routerLogin);
+app.use('/area', bearerAuth, routerArea);
 
 export default app;
