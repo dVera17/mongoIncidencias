b029f60acd30016ed4c01b53e0844cea26d77a14
dVera17 <dsvera174@gmail.com>
2023-08-24 23:27:29 -0500

    feat: :sparkles: Implementacion de los endpoints creados
    Implementacion de los endpoints creados

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { config } from "dotenv";
 import routerLogin from './routes/login.routes.js';
 import routerArea from './routes/area.routes.js';
+import routerDispositivo from './routes/dispositivo.routes.js';
 import passportHelper from './config/passportConfig.js';
 config();
 const app = express();
@@ -12,5 +13,6 @@ const bearerAuth = passportHelper.authenticate('bearer', { session: false })
 
 app.use('/login', routerLogin);
 app.use('/area', bearerAuth, routerArea);
+app.use('/dispositivo', bearerAuth, routerDispositivo);
 
 export default app;
